c60211d28ee940fd51c3a35f95c51fe0534184f8
IronTwist <47633722+IronTwist@users.noreply.github.com>
2021-05-09 02:40:09 +0300

    Adding NoteController

---
+++ Data/Migrations/20210508225309_UpdateNote.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agenda.Data.Migrations
+{
+    public partial class UpdateNote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_notes",
+                table: "notes");
+
+            migrationBuilder.RenameTable(
+                name: "notes",
+                newName: "Notes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notes",
+                table: "Notes",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notes",
+                table: "Notes");
+
+            migrationBuilder.RenameTable(
+                name: "Notes",
+                newName: "notes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_notes",
+                table: "notes",
+                column: "Id");
+        }
+    }
+}
